2a085e00ca70360056dc23048a88721b5989b45a
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-02-14 14:35:39 +0530

    82 Introducing the Dimensions API

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -1,14 +1,16 @@
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 
 import Colors from "../../constants/colors";
 function Card({ children }) {
   return <View style={styles.card}>{children}</View>;
 }
+
+const deviceWidth = Dimensions.get("window").width;
 export default Card;
 
 const styles = StyleSheet.create({
   card: {
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     padding: 16,
     marginHorizontal: 24,
     borderRadius: 8,
--- components/game/NumbersContainer.js
+++ components/game/NumbersContainer.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 
 import Colors from "../../constants/colors";
 
@@ -10,13 +10,15 @@ function NumberContainer({ children }) {
   );
 }
 
+const deviceWidth = Dimensions.get("window").width;
+
 export default NumberContainer;
 
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accentColor,
-    padding: 24,
+    padding: deviceWidth < 380 ? 12 : 24,
     borderRadius: 8,
     margin: 24,
     alignItems: "center",
@@ -25,7 +27,6 @@ const styles = StyleSheet.create({
   numberText: {
     fontFamily: "open-sans-bold",
     color: Colors.accentColor,
-
     fontSize: 30,
   },
 });
